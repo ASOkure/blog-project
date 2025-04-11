e1223472a8ee3321ce13c0938098de8ef9437d07
akanimo sam okure <akan.okure@gmail.com>
2025-04-11 21:55:52 +0100

    implement create post and home page components

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,3 +1,28 @@
+import { PostCard } from "../components";
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  const posts = [
+    {
+      id: 1,
+      title: "Lorem ipsum dolor sit amet",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quidem magnam vitae, deserunt facilis, commodi accusamus doloribus fugiat, soluta debitis illo nostrum quo tempora consequatur quas illum nobis laboriosam recusandae distinctio nihil ratione. Minima reprehenderit maiores fugiat cupiditate dolorem, consequuntur asperiores nostrum voluptatem laboriosam temporibus obcaecati, nesciunt beatae possimus tenetur.",
+      author: "Sam",
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum dolor",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quidem magnam vitae, deserunt facilis, commodi accusamus doloribus fugiat, soluta debitis illo nostrum quo tempora consequatur quas illum nobis laboriosam recusandae distinctio nihil ratione. Minima reprehenderit maiores fugiat cupiditate dolorem, consequuntur asperiores nostrum voluptatem.",
+      author: "Sam",
+    },
+  ];
+
+  return (
+    <section>
+      {posts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </section>
+  );
 };
