7b896a6d93cf6b554261feef7c466f64c32af8f4
akanimo sam okure <akan.okure@gmail.com>
2025-04-11 21:20:29 +0100

    implement header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/logo.png";
+
 export const Header = () => {
+  const isAuth = true;
+
   return (
     <header>
       <Link to="/" className="logo">
@@ -8,11 +11,23 @@ export const Header = () => {
         <span>WriteNode</span>
       </Link>
       <nav className="nav">
-        <Link to="/" className="link">
-          {" "}
+        <NavLink to="/" className="link" end>
           Home
-        </Link>
-        <button className="auth"> Login</button>
+        </NavLink>
+        {isAuth ? (
+          <>
+            <NavLink to="/create" className="link">
+              Create
+            </NavLink>
+            <button className="auth">
+              <i className="bi bi-box-arrow-right"></i> Logout
+            </button>
+          </>
+        ) : (
+          <button className="auth">
+            <i className="bi bi-google"></i> Login
+          </button>
+        )}
       </nav>
     </header>
   );
