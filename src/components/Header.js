3f48f6f8d1e033ee39720d9077aab9d291a6d798
akanimo sam okure <akan.okure@gmail.com>
2025-04-11 21:12:55 +0100

    add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom";
+import Logo from "../assets/logo.png";
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Link to="/" className="logo">
+        <img src={Logo} alt="WriteNode Logo" />
+        <span>WriteNode</span>
+      </Link>
+      <nav className="nav">
+        <Link to="/" className="link">
+          {" "}
+          Home
+        </Link>
+        <button className="auth"> Login</button>
+      </nav>
+    </header>
+  );
 };
