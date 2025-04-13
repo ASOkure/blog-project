6962a485e23dbccf7ef7b6de0d11b4d707850eee
akanimo sam okure <akan.okure@gmail.com>
2025-04-13 22:40:02 +0100

    set up and configure cloud firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDlZSFERp5qmzct1daU5iv7tEao9bU-Vgs",
+  authDomain: "mtsblog-21631.firebaseapp.com",
+  projectId: "mtsblog-21631",
+  storageBucket: "mtsblog-21631.firebasestorage.app",
+  messagingSenderId: "1054730271695",
+  appId: "1:1054730271695:web:ea85b9f092ec58c14530ba",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
